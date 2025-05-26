b94eabcd86f727d1b0f5f8ec8ff5192aa57f6213
takahashi yuuki <te200394@gmail.com>
2025-05-26 11:04:39 +0900

    kadai6 commit

--- src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookController.java
@@ -1,14 +1,15 @@
 package jp.te4a.spring.boot.myapp6.mybootapp6;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
 
-
+@RequestMapping("")
+@Controller
 public class BookController {
     @Autowired
     BookService bookService;
