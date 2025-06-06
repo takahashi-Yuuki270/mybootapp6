1d6eb26a8c5daab998fe525a28ce8da512fd7838
takahashi yuuki <te200394@gmail.com>
2025-06-06 15:07:26 +0900

    kadai11 commit

--- src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookController.java
@@ -1,14 +1,18 @@
-package jp.te4a.spring.boot.myapp9.mybootapp9;
+package jp.te4a.spring.boot.myapp11.mybootapp11;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated; 
+
 
 @RequestMapping("/books")
 @Controller
@@ -26,6 +30,15 @@ public class BookController {
  return "books/list";
  }
  @PostMapping(path="create")
+ String create(@Validated BookForm form,BindingResult result, Model model) {
+    if(result.hasErrors()){
+       return list(model);
+    }
+    bookService.update(form);
+    return "redirect:/books";
+}
+
+   
  String create(BookForm form, Model mode) {
  bookService.create(form);
  return "redirect:/books";
@@ -47,7 +60,14 @@ public class BookController {
  return "redirect:/books";
  }
  @PostMapping(path = "edit", params = "goToTop")
- String goToTop() {
+  String edit(@RequestParam Integer id, @Validated BookForm form, BindingResult result) {
+    if(result.hasErrors()) {
+     return editForm(id, form);
+     }
+     bookService.update(form);
+     return "redirect:/books";
+    }
+String goToTop() {
  return "redirect:/books";
  }
 }
---
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/TestValid.java
@@ -0,0 +1,25 @@
+package jp.te4a.spring.boot.myapp11.mybootapp11;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+
+
+@Documented
+@Constraint(validatedBy = TestValidator.class)
+@Target({ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface TestValid {
+    String param();
+    String message() default  "input other than  {param}.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default{};
+}
